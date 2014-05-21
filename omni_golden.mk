d4065af1ba5a4a746c44f245b4bffb654543fde8
andi34 <skate4life@gmx.de>
2014-05-21 21:40:34 +0200

    Golden: general update / rework

--- omni_golden.mk
+++ omni_golden.mk
@@ -1,6 +1,7 @@
 #
 # Copyright (C) 2013 The Android Open Source Project
 # Copyright (C) 2013 Óliver García Albertos (oliverarafo@gmail.com)
+# Copyright (C) 2014 OmniROM Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,16 +16,21 @@
 # limitations under the License.
 #
 
-# Inherit from Omni custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit Omni GSM telephony parts
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
+# Inherit common Omni configurations
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit device configuration
-$(call inherit-product, device/samsung/golden/golden.mk)
+$(call inherit-product, device/samsung/golden/device.mk)
 
 # Device identifier
+PRODUCT_MODEL := GT-I8190
+PRODUCT_BRAND := samsung
 PRODUCT_NAME := omni_golden
 PRODUCT_DEVICE := golden
-PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
-PRODUCT_MODEL := GT-I8190
