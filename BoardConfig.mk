b974f0f682b9ab997c30b92e6f7f16c11fe576a8
andi34 <skate4life@gmx.de>
2014-08-26 21:40:35 +0200

    DERP: fix compileing

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -142,7 +142,7 @@ BOARD_SEPOLICY_UNION += \
 # Recovery
 BOARD_UMS_LUNFILE := "/sys/devices/platform/musb-ux500.0/musb-hdrc/gadget/lun0/file"
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/musb-ux500.0/musb-hdrc/gadget/lun%d/file"
-TARGET_RECOVERY_FSTAB := device/samsung/golden/rootdir/fstab.samsunggolden
+TARGET_RECOVERY_FSTAB := device/samsung/golden/root/fstab.samsunggolden
 RECOVERY_FSTAB_VERSION := 2
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/golden/recovery/graphics.c
