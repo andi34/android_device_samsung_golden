909169cc194a5146e53a3e6e2227a23d8fac773a
andi34 <skate4life@gmx.de>
2014-08-28 22:11:13 +0200

    golden: TWRP: use "TW_INCLUDE_JB_CRYPTO"

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,11 +155,4 @@ TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
 TW_BRIGHTNESS_PATH := "/sys/devices/mcde_disp_s6e63m0_dsi.0/backlight/panel/brightness"
 TW_MAX_BRIGHTNESS := 255
-TW_INCLUDE_CRYPTO_SAMSUNG := true
-TW_INCLUDE_CRYPTO := true
-TW_CRYPTO_FS_TYPE := "ext4"
-TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p25"
-TW_CRYPTO_MNT_POINT := "/data"
-TW_CRYPTO_FS_OPTIONS := "noatime,nosuid,nodev,discard,noauto_da_alloc,journal_async_commit,errors=panic    wait,check"
-TW_CRYPTO_FS_FLAGS := "0x00000406"
-TW_CRYPTO_KEY_LOC := "footer"
+TW_INCLUDE_JB_CRYPTO := true
