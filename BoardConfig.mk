6c51299d3ec458c725b262bad3aae7d7348296c3
Andreas Blaesius <skate4life@gmx.de>
2015-10-20 16:49:05 +0200

    golden: TWRP: set TW_SCREEN_BLANK_ON_BOOT to true
    Fixes black screen on boot if turned off automatical sleep mode
    
    Change-Id: I16b514e53ff558cac8b992814db1f3b5a0ff3f75

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,6 +163,7 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/musb-ux500.0/musb-hdrc
 # TWRP Recovery defines
 TW_BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/golden/recovery/twrp-graphics.c
 TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_SCREEN_BLANK_ON_BOOT := true
 TW_THEME := portrait_mdpi
 RECOVERY_SDCARD_ON_DATA := true
 TW_INTERNAL_STORAGE_PATH := "/data/media"
