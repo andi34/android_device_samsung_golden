a9748f7502c7056f9a0865823a9f91916d1b8897
Andreas Blaesius <skate4life@gmx.de>
2016-02-06 21:13:14 +0100

    golden: update shbootimg.mk for Omni 6.0

--- shbootimg.mk
+++ shbootimg.mk
@@ -7,17 +7,23 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES)
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
 	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
 
-$(recovery_uncompressed_ramdisk): $(MINIGZIP) \
-		$(TARGET_RECOVERY_ROOT_TIMESTAMP)
+$(recovery_uncompressed_ramdisk): $(MKBOOTFS) \
+		$(INSTALLED_BOOTIMAGE_TARGET) \
+		$(INTERNAL_RECOVERYIMAGE_FILES) \
+		$(recovery_initrc) $(recovery_sepolicy) $(recovery_kernel) \
+		$(INSTALLED_2NDBOOTLOADER_TARGET) \
+		$(recovery_build_prop) $(recovery_resource_deps) $(recovery_root_deps) \
+		$(recovery_fstab) \
+		$(RECOVERY_INSTALL_OTA_KEYS)
 	mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/lib/modules
 	cp -f $(TARGET_OUT)/lib/modules/j4fs.ko $(TARGET_RECOVERY_ROOT_OUT)/lib/modules/j4fs.ko
 	cp -f $(TARGET_OUT)/lib/modules/param.ko $(TARGET_RECOVERY_ROOT_OUT)/lib/modules/param.ko
-	@echo -e ${CL_CYN}"----- Making uncompressed recovery ramdisk ------"${CL_RST}
-	$(MKBOOTFS) $(TARGET_RECOVERY_ROOT_OUT) > $@
+	$(call build-recoveryramdisk)
+	@echo -e ${PRT_IMG}"----- Making uncompressed recovery ramdisk ------"${CL_RST}
+	$(hide) $(MKBOOTFS) $(TARGET_RECOVERY_ROOT_OUT) > $@
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
 		$(recovery_kernel) \
-		$(recovery_uncompressed_ramdisk) \
 		$(recovery_ramdisk)
 	@echo -e ${CL_CYN}"----- Compressing recovery ramdisk using LZMA ------"${CL_RST}
 	$(hide) rm -f $(OUT)/ramdisk-recovery.cpio.lzma
