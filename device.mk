1ce100ade76a83beac38d5ed9c89dbba3a251dfd
Andreas Blaesius <skate4life@gmx.de>
2016-03-28 11:43:27 +0200

    golden: get rid of custom init.environ.rc
    Change-Id: I04e01e06d63862e9b52a9d98c32b7831b583fee9

--- device.mk
+++ device.mk
@@ -26,7 +26,6 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 # Ramdisk
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.samsunggolden.rc:root/init.samsunggolden.rc \
-    $(LOCAL_PATH)/rootdir/init.environ.rc:root/init.environ.rc \
     $(LOCAL_PATH)/rootdir/ueventd.samsunggolden.rc:root/ueventd.samsunggolden.rc \
     $(LOCAL_PATH)/rootdir/init.samsunggolden.usb.rc:root/init.samsunggolden.usb.rc \
     $(LOCAL_PATH)/rootdir/fstab.samsunggolden:root/fstab.samsunggolden \
